f4a1cb2f305b9afb549f9bc56d819c469365cb4e
Charles Morris Jr <cmorrisjr777@gmail.com>
2022-11-14 14:35:05 -0500

    JS120 Practice Problems - Constructors and Prototypes

---
+++ lesson_3/Practice Problems - Constructors and Prototypes - Problem 7.js
@@ -0,0 +1,18 @@
+let ninjaA;
+
+{
+  const Ninja = function() {
+    this.swung = false;
+  };
+
+  ninjaA = new Ninja();
+}
+
+// create a `ninjaB` object here; don't change anything else
+
+// let ninjaB = {};
+// Object.setPrototypeOf(ninjaB, Object.getPrototypeOf(ninjaA));
+
+let ninjaB = new ninjaA.constructor();
+
+console.log(ninjaA.constructor === ninjaB.constructor); // => true
