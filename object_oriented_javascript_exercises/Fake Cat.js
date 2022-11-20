5fc84a22391464c67ad14f36885ad79761bf8432
Charles Morris Jr <cmorrisjr777@gmail.com>
2022-11-20 01:57:57 -0500

    JS120 OO JavaScript Exercises

---
+++ object_oriented_javascript_exercises/Fake Cat.js
@@ -0,0 +1,19 @@
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+  speaks() {
+    return `${this.name} says meowwww.`;
+  }
+}
+
+class Tabby extends Cat {
+  constructor() {
+    super();
+  }
+}
+
+let fakeCat = new Tabby();
+console.log(fakeCat instanceof Cat); // logs true
+console.log(fakeCat.name);           // logs undefined
+console.log(fakeCat.speaks());       // logs undefined says meowwww.
