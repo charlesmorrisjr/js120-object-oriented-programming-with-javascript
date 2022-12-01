3ada5bfab8d9272c9ff29995d06f899bd3fd7dd9
Charles Morris Jr <cmorrisjr777@gmail.com>
2022-12-01 17:12:45 -0500

    JS120 OO Exercises - Extra

--- object_oriented_javascript_exercises/Constructors With Prototypes/Animals.js
+++ object_oriented_javascript_exercises/Constructors With Prototypes/Animals.js
@@ -1,16 +1,42 @@
-class Animal {
-  constructor(name, age, legs, species, status) {
-    this.name = name;
-    this.age = age;
-    this.legs = legs;
-    this.species = species;
-    this.status = status;
-  }
-  introduce() {
-    return `Hello, my name is ${this.name} and I am ${this.age} years old and ${this.status}.`;
-  }
+function Animal(name, age, legs, species, status) {
+  this.name = name;
+  this.age = age;
+  this.legs = legs;
+  this.species = species;
+  this.status = status;
 }
+
+Animal.prototype.introduce = function() {
+  return `Hello, my name is ${this.name} and I am ${this.age} years old and ${this.status}.`;
+};
+
+function Cat(name, age, status) {
+  Animal.call(this, name, age, 4, 'cat', status);
+}
+
+Cat.prototype = Object.create(Animal.prototype);
+Cat.prototype.constructor = Cat;
+
+Cat.prototype.introduce = function() {
+  return Animal.prototype.introduce.call(this) + ' Meow meow!';
+};
+
+function Dog(name, age, status, master) {
+  Animal.call(this, name, age, 4, 'dog', status);
+  this.master = master;
+}
+
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+
+Dog.prototype.greetMaster = function() {
+  return `Hello ${this.master}! Woof woof!`;
+};
 
 let cat = new Cat("Pepe", 2, "happy");
 console.log(cat.introduce() === "Hello, my name is Pepe and I am 2 years old and happy. Meow meow!");
 // logs true
+
+let dog = new Dog('Fido', 3, "sad", 'John');
+console.log(dog.introduce());
+console.log(dog.greetMaster());
--- object_oriented_javascript_exercises/OLOO/Animals.js
+++ object_oriented_javascript_exercises/OLOO/Animals.js
@@ -1,16 +1,45 @@
-class Animal {
-  constructor(name, age, legs, species, status) {
+let Animal = {
+  init(name, age, legs, species, status) {
     this.name = name;
     this.age = age;
     this.legs = legs;
     this.species = species;
     this.status = status;
-  }
+    return this;
+  },
+
   introduce() {
     return `Hello, my name is ${this.name} and I am ${this.age} years old and ${this.status}.`;
   }
-}
+};
 
-let cat = new Cat("Pepe", 2, "happy");
+let Cat = Object.create(Animal);
+
+Cat.init = function(name, age, status) {
+  Animal.init.call(this, name, age, 4, 'cat', status);
+  return this;
+};
+
+Cat.introduce = function() {
+  return Animal.introduce.call(this) + ' Meow meow!';
+};
+
+let Dog = Object.create(Animal);
+
+Dog.init = function(name, age, status, master) {
+  Animal.init.call(this, name, age, 4, 'dog', status);
+  this.master = master;
+  return this;
+};
+
+Dog.greetMaster = function() {
+  return `Hello ${this.master}! Woof woof!`;
+};
+
+let cat = Object.create(Cat).init("Pepe", 2, "happy");
 console.log(cat.introduce() === "Hello, my name is Pepe and I am 2 years old and happy. Meow meow!");
 // logs true
+
+let dog = Object.create(Dog).init('Fido', 3, "sad", 'John');
+console.log(dog.introduce());
+console.log(dog.greetMaster());
